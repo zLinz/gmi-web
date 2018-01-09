d763e72855aae31e56060a17ac3472659c6d16a3
linzhizhaoooo <linzhizhao790086754@gmail.com>
2018-01-09 12:24:00 +0800

    update src

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,4 +1,40 @@
-
+/**
+ * @description Index page logic
+ * @author Liam
+ */
+var Global = require('tool/global');
+var Host = require('tool/host');
+require('tool/error');
 $(function () {
-    console.log ("index loaded!")
+    var app = new Vue({
+        el: '.index-page',
+        data: {
+
+        },
+        mounted: function () {
+            new Swiper ('.swiper-container', {
+                direction: 'horizontal',
+                loop: true,
+                effect: 'slide',
+                zoom: 1,
+                freeMode: false,
+                autoplay: {
+                    delay: 2000,
+                    disableOnInteraction: false
+                },
+                scrollbar: {
+                    draggable: true
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+
+            })
+        },
+        methods: {
+
+        }
+    });
+
 })
